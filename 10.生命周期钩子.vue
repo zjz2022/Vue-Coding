334edd2bcbabb3dad5683e8b8b677411dcf67cf4
zjz2022 <zjz15019702849@gmail.com>
2024-03-17 23:00:00 +0800

    vue coding update

---
+++ 10.生命周期钩子.vue
@@ -0,0 +1,27 @@
+<!-- Child.vue -->
+<script setup lang="ts">
+import { onMounted, inject, onUnmounted } from "vue"
+
+const timer = inject("timer")
+const count = inject("count")
+
+onMounted(() => {
+  // 切换子组件时, 定时器将不正常工作, 让我们来修复它 . 
+  timer.value = window.setInterval(() => {
+    count.value++
+  }, 1000)
+})
+onUnmounted(() => {
+  window.clearInterval(timer.value)
+  timer.value = null
+})
+
+</script>
+
+<template>
+  <div>
+    <p>
+      Child Component: {{ count }}
+    </p>
+  </div>
+</template>
---
+++ 13.DOM传送门.vue
@@ -0,0 +1,19 @@
+<!-- Teleport组件 
+  <Teleport> 是一个内置组件，它可以将一个组件内部的一部分模板“传送”到该组件的 DOM 结构外层的位置去。
+  https://cn.vuejs.org/guide/built-ins/teleport.html -->
+
+<!-- Vue.js提供了一个内置组件，将其插槽内容渲染到另一个DOM，成为该DOM的一部分。
+
+你知道它是什么吗 ? 让我们试试👇: -->
+<script setup>
+
+const msg = "Hello World"
+
+</script>
+
+<template>
+  <!-- Renders it to a child element of the `body` -->
+  <Teleport to="body">
+    <span>{{ msg }}</span>
+  </Teleport>
+</template>
